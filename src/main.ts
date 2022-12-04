7fc34cdf9d69a2ff8dbf9f4216444c1e12affda6
chenpeng <chenpeng@kaadas.com>
2022-12-04 16:24:49 +0800

    feat: Close the right-click menu

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,6 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 
+document.addEventListener('contextmenu', event => event.preventDefault());
+
 createApp(App).mount("#app");
